968b1342e52a0f0ff3fe20b17220451fc30e8ada
coach4u <coach.rhca@gmail.com>
2025-11-05 12:20:22 +0530

    Add the provider section in the backend file

--- terraform_asg/backend.tf
+++ terraform_asg/backend.tf
@@ -7,4 +7,15 @@ terraform {
         encrypt        = true
       
     }
+    required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+
+provider "aws" {
+  region = var.region
+}
 }
