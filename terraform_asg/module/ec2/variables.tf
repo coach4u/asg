456240a223bdce36d827fe3a5dbaf8f337851615
coach4u <coach.rhca@gmail.com>
2025-11-05 11:09:57 +0530

    added terraform files

---
+++ terraform_asg/module/ec2/variables.tf
@@ -0,0 +1,5 @@
+variable "ami_id" { type = string }
+variable "instance_type" {type = sting }
+variable "subnet_id" { type = string }
+variable "key_name" { type = string }
+variable "environment" { type = string }
