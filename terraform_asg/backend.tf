24c2ccb007a6292240063a82e1f6da90d798e8d1
coach4u <34412145+coach4u@users.noreply.github.com>
2025-11-11 14:35:45 +0530

    Remove unnecessary provider block in backend.tf

--- terraform_asg/backend.tf
+++ terraform_asg/backend.tf
@@ -7,15 +7,8 @@ terraform {
         encrypt        = true
     }
     }
-    required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+   
   
 
 
-provider "aws" {
-  region = var.region
-}
-}
+
