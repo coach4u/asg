4461ccec8f8a3872d87c8340e00b02e0522c1c52
coach4u <34412145+coach4u@users.noreply.github.com>
2025-11-11 14:23:15 +0530

    Update backend.tf

--- terraform_asg/backend.tf
+++ terraform_asg/backend.tf
@@ -5,14 +5,14 @@ terraform {
         region = "us-east-1"
         dynamodb_table ="terraform-lock-table"
         encrypt        = true
-      
+    }
     }
     required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-  }
+  
 
 
 provider "aws" {
