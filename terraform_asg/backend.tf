f1ce16d5f2f60b1b452fe02b339fa98e762f13a0
coach4u <34412145+coach4u@users.noreply.github.com>
2025-11-11 17:12:21 +0530

    Update backend.tf

--- terraform_asg/backend.tf
+++ terraform_asg/backend.tf
@@ -1,11 +1,11 @@
 terraform {
     backend "s3" {
-        bucket = "terraform-buc01"
+        bucket = "terraform-state-buc-001"
         key = "ec2/terraform.tfstate"
         region = "us-east-1"
         dynamodb_table ="terraform-lock-table"
         encrypt        = true
-    }
+     }
     }
    
   
