31659d2c5e5c219a689e78ac50fa08919016b9da
coach4u <34412145+coach4u@users.noreply.github.com>
2025-11-11 17:42:03 +0530

    Update variables.tf

--- terraform_asg/variables.tf
+++ terraform_asg/variables.tf
@@ -2,5 +2,5 @@ variable "ami_id" { type = string }
 variable "instance_type" {type = string }
 variable "subnet_id" { type = string }
 variable "key_name" { type = string }
-variable "environment" { type = string }
+variable "env" { type = string }
 variable "security_group_id" { type = string }
