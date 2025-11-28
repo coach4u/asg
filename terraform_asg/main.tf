921814c67cf116d32525f4ed3c56a3bd90889668
coach4u <34412145+coach4u@users.noreply.github.com>
2025-11-28 18:46:36 +0530

    Update main.tf

--- terraform_asg/main.tf
+++ terraform_asg/main.tf
@@ -13,4 +13,17 @@ output "instance_ids" {
   value = module.ec2.instance_ids
 }
 
+output "private_ips" {
+  value = module.ec2.private_ips
+}
+
+output "public_ips" {
+  value = module.ec2.public_ips
+}
+output "public_dns" {
+  value = module.ec2.public_dns
+}
+
+
+
 
