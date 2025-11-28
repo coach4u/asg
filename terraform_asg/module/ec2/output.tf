66afdbffcef0e1d08b7840cf1f0e695473064dba
coach4u <34412145+coach4u@users.noreply.github.com>
2025-11-28 18:49:02 +0530

    Update output.tf

--- terraform_asg/module/ec2/output.tf
+++ terraform_asg/module/ec2/output.tf
@@ -1,14 +1,14 @@
 output "instance_ids" {
     value = { for instance_name, instance in aws_instance.this :
-    instance_name => instance.id
+    instance_name => instance.id }
   }
 output "private_ips" {
      value={ for instance_name,instance in aws_instance.this: 
-     instance_name=> instance.private_ip
+     instance_name=> instance.private_ip }
 }
 output "public_ip" {
      value={for instance_name,instance in aws_instance.this:
-     instance_name=> instance.public_ip
+     instance_name=> instance.public_ip }
 }
 output "public_dns" {
   value = { for name, inst in aws_instance.this : name => inst.public_dns }
