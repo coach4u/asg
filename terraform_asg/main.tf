56b53be87d390e4c7fb8c91ece945990bb06e5de
coach4u <34412145+coach4u@users.noreply.github.com>
2025-11-28 14:42:02 +0530

    Add EC2 module and output for instance ID

--- terraform_asg/main.tf
+++ terraform_asg/main.tf
@@ -1,1 +1,13 @@
+module "ec2" {
+  source            = "./module/ec2"
+  ami_id            = var.ami_id
+  instance_type     = var.instance_type
+  subnet_id         = var.subnet_id
+  security_group_id = var.security_group_id
+  key_name          = var.key_name
+  env = var.env
+}
 
+output "instance_id" {
+  value = module.ec2.instance_id
+}
