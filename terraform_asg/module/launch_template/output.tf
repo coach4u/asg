0c90a553e5a4ee54263230b7c0c3b316b61092b3
coach4u <34412145+coach4u@users.noreply.github.com>
2025-11-30 12:05:45 +0530

    Create output.tf

---
+++ terraform_asg/module/launch_template/output.tf
@@ -0,0 +1,11 @@
+output "launch_template_id" {
+  value = aws_launch_template.lt.id
+}
+
+output "launch_template_arn" {
+  value = aws_launch_template.lt.arn
+}
+
+output "launch_template_latest_version" {
+  value = aws_launch_template.lt.latest_version
+}
