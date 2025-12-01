84a8e0479033751698f550c111f62aa554f9f17d
coach4u <34412145+coach4u@users.noreply.github.com>
2025-12-01 17:06:38 +0530

    Update variable.tf

--- terraform_asg/stacks/launch_template/variable.tf
+++ terraform_asg/stacks/launch_template/variable.tf
@@ -1,1 +1,15 @@
+variable "env" {
+  type = string
+}
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "name" { type = string }
+variable "ami_id" { type = string }
+variable "instance_type" { type = string }
+variable "security_group_ids" { type = list(string) }
+variable "subnet_id" { type = string, default = "" }
+variable "user_data_file" { type = string, default = "" }
+variable "key_name" { type = string, default = "" }
 
