5cc3504b2d3480a66299af486c6a11debff8fe93
coach4u <34412145+coach4u@users.noreply.github.com>
2025-12-01 12:17:54 +0530

    Update main.tf

--- terraform_asg/module/launch_template/main.tf
+++ terraform_asg/module/launch_template/main.tf
@@ -73,14 +73,14 @@ resource "aws_launch_template" "lt" {
 }
 
 
-output "launch_template_id" {
-  value = aws_launch_template.lt.id
-}
+#output "launch_template_id" {
+#  value = aws_launch_template.lt.id
+#}
 
-output "launch_template_arn" {
-  value = aws_launch_template.lt.arn
-}
+#output "launch_template_arn" {
+#  value = aws_launch_template.lt.arn
+#}
 
-output "launch_template_latest_version" {
-  value = aws_launch_template.lt.latest_version
-}
+#output "launch_template_latest_version" {
+#  value = aws_launch_template.lt.latest_version
+#}
